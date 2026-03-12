01a8cc6b5fc16f0a6bda9c9fd7f08284d2a911f3
vvvvvvss <21varsha.shri.m@gmail.com>
2026-03-12 23:06:04 +0530

    because the deadline extended

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/design_disaster_RideAway/', // Ensure the slashes are there!
+  // THIS IS THE FIX: Tell Vite your repo name
+  base: '/design_disaster_RideAway/', 
+  server: {
+    port: 3000
+  }
 })
